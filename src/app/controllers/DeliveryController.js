704a776e725492d31e6c84eeff32a90972a9a49d
Thiago Amancio <tjamancio@hotmail.com>
2020-02-28 15:32:27 -0300

    Implementado fila para o envio de email

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,4 +1,14 @@
+import * as Yup from 'yup';
 import Delivery from '../models/Delivery';
+import DeliveryMan from '../models/DeliveryMan';
+import Queue from '../../lib/Queue';
+import NewDeliveryMail from '../jobs/NewDeliveryMail';
+
+const schema = Yup.object().shape({
+  recipient_id: Yup.number().required(),
+  deliveryman_id: Yup.number().required(),
+  product: Yup.string().required(),
+});
 
 class DeliveryController {
   async index(req, res) {
@@ -12,11 +22,26 @@ class DeliveryController {
   }
 
   async store(req, res) {
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const deliveryman = await DeliveryMan.findByPk(req.body.deliveryman_id);
     const delivery = await Delivery.create(req.body);
+
+    await Queue.add(NewDeliveryMail.key, {
+      deliveryman,
+      delivery,
+    });
+
     return res.json(delivery);
   }
 
   async update(req, res) {
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const delivery = await Delivery.findByPk(req.params.id);
     await delivery.update(req.body);
     return res.json(delivery);
